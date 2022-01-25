2d5876780b2ada8332d3f13f7425317fed0a7ec6
ravikanth chenna <chenna.blr@gmail.com>
2022-01-25 15:29:15 +0530

    little modification in script

--- automation.sh
+++ automation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #!/bin/bash
-if [ $(/etc/init.d/apache2 status | grep -v grep | grep 'Apache2 is running' | wc -l) > 0 ]
+if [ $(/etc/init.d/apache2 status | grep 'Apache2 is running' | wc -l) > 0 ]
 then
  echo "Process is running."
 else
